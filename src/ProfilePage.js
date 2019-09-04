66457e3b4e5db256b450262f3c31d3a8ea38da0b
Sam Chan <spc28@duke.edu>
2019-09-04 13:29:14 -0400

    Profile page looks better

--- src/ProfilePage.js
+++ src/ProfilePage.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
+import Button from '@material-ui/core/Button';
 
 function ProfilePage() {
   return (
-    <div>
-      This is a profile page
+    <div className="App">
+      <header className="App-header">
+        <p>
+          This is a profile page
+        </p>
+        <Link to='/'>
+        <Button variant="contained" color="primary">
+          Home
+
+          </Button>
+        </Link>
+      </header>
+
     </div>
   );
 }
